41060edd77dd214b63e398390f9c6679209f5102
mishel254 <sonimichie@gmail.com>
2020-08-21 19:03:45 +0300

    selecting elements  from the Dom

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ let female = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 let male = ['Kwasi','Kwadwoa','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-
+let input = document.getElementById('input');
+let gender = document.getElementById('gender');
+let  submit = document.getElementById('submit');
