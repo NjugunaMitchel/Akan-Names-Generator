a3caee24b5b3b8a11132da387fe4792c10004d87
mishel254 <sonimichie@gmail.com>
2020-08-21 19:04:45 +0300

    assigning data to variables

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturda
 let input = document.getElementById('input');
 let gender = document.getElementById('gender');
 let  submit = document.getElementById('submit');
+
+
+submit.addEventListener('click',(e) => {
+    e.preventDefault();     
+    let  data = input.value;
+    //century
+    let c = data.slice(0, 2);
+    let cc = parseInt(c);
+    //year
+    let y = data.slice(2, 4);
+    let yy = parseInt(y);
+    //month
+    let m = data.slice(8, 10);
+    let mm = parseInt(m);
+    //date
+    let ddd = data.slice(5, 7);
+    let dd = parseInt(ddd)
+
+    //CALCULATE THE DAY
+    let d = Math.floor((((( cc/4 )- 2 )* (cc - 1 ))+(( (5 * yy) / 4 ))+(( (26 * ( mm + 1 ) )/ 10)) + dd) % 7);
